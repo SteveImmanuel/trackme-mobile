492346c9fd42cf2b91afc948465ce9f57c3cf7e2
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-09 01:22:57 +0700

    Implement own splash screen

---
+++ lib/screens/splash.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:trackme_mobile/utilities/api.dart';
+import 'package:trackme_mobile/screens/home.dart';
+import 'package:trackme_mobile/screens/auth.dart';
+import 'package:trackme_mobile/main.dart';
+
+class Splash extends StatefulWidget {
+  static String route = '/';
+
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  Future<void> _initializeApp() async {
+    await initializeApi();
+    Map<String, dynamic> authCheckResult = await authCheck();
+
+    if (authCheckResult['code'] == 200) {
+      MainApp.navKey.currentState?.pushNamed(Home.route);
+    } else {
+      MainApp.navKey.currentState?.pushNamed(Auth.route);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeApp();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: 'trackmeBanner',
+                child: Image.asset(
+                  'assets/images/trackme_banner.png',
+                  width: MediaQuery.of(context).size.width * 0.7,
+                ),
+              )
+            ],
+          ),
+          CircularProgressIndicator(
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
