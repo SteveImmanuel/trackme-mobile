619acadcf798f1908803e614ff698c6ff0459356
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-08 11:48:46 +0700

    Add platform type

--- lib/models/bot_channel.dart
+++ lib/models/bot_channel.dart
@@ -3,19 +3,28 @@ class BotChannel {
   String type;
   String photoUrl;
   String displayName;
+  String platform;
 
-  BotChannel(this.id, this.type, this.photoUrl, this.displayName);
+  BotChannel(
+    this.id,
+    this.type,
+    this.photoUrl,
+    this.displayName,
+    this.platform,
+  );
 
   BotChannel.fromJson(Map<String, dynamic> data)
       : id = data['id'],
         type = data['type'],
         photoUrl = data['photo_url'],
-        displayName = data['display_name'];
+        displayName = data['display_name'],
+        platform = data['platform'];
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'type': type,
         'photo_url': photoUrl,
-        'display_name': displayName
+        'display_name': displayName,
+        'platform': platform
       };
 }
