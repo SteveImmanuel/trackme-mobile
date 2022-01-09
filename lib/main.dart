5b9c7b843b881a71b56c2b9379c06b0005942b9c
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-01-09 14:02:00 +0700

    Configure args for named route

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:trackme_mobile/screens/home.dart';
 import 'package:trackme_mobile/screens/auth.dart';
 import 'package:trackme_mobile/screens/choose_location.dart';
 import 'package:trackme_mobile/screens/splash.dart';
+import 'package:trackme_mobile/utilities/route_arguments.dart';
 
 void main() {
   runApp(const MainApp());
@@ -30,12 +31,35 @@ class MainApp extends StatelessWidget {
       navigatorKey: navKey,
       initialRoute: Splash.route,
       theme: theme,
-      routes: {
-        Splash.route: (context) => const Splash(),
-        Auth.route: (context) => const Auth(),
-        Home.route: (context) => const Home(),
-        ChooseLocation.route: (context) => const ChooseLocation()
+      onGenerateRoute: (settings) {
+        if (settings.name == ChooseLocation.route) {
+          final args = settings.arguments as LocationArgs;
+
+          return MaterialPageRoute(
+            builder: (context) {
+              return ChooseLocation(
+                callback: args.callback,
+                currentLocationList: args.currentLocationList,
+                currentIndex: args.currentIndex,
+              );
+            },
+          );
+        } else if (settings.name == Splash.route) {
+          return MaterialPageRoute(builder: (context) => const Splash());
+        } else if (settings.name == Auth.route) {
+          return MaterialPageRoute(builder: (context) => const Auth());
+        } else if (settings.name == Home.route) {
+          return MaterialPageRoute(builder: (context) => const Home());
+        }
+        assert(false, 'Need to implement ${settings.name}');
+        return null;
       },
+      // routes: {
+      //   Splash.route: (context) => const Splash(),
+      //   Auth.route: (context) => const Auth(),
+      //   Home.route: (context) => const Home(),
+      //   ChooseLocation.route: (context) => const ChooseLocation()
+      // },
     );
   }
 }
