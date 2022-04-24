911a9c351520a7ceb90b20c4d09ca7dfe2f857d7
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-04-24 12:43:29 +0900

    Update navigation, integrate profile

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,8 +6,9 @@ import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:trackme/screens/choose_location.dart';
 import 'package:trackme/widgets/bot_channels.dart';
 import 'package:trackme/widgets/locations.dart';
-import 'package:trackme/widgets/profile.dart';
+import 'package:trackme/widgets/tracking.dart';
 import 'package:trackme/widgets/aliases.dart';
+import 'package:trackme/widgets/profile.dart';
 import 'package:trackme/models/user.dart';
 import 'package:trackme/utilities/api.dart';
 import 'package:trackme/utilities/route_arguments.dart';
@@ -34,7 +35,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
+  int _currentIndex = 2;
   String _newAlias = '';
   final User _user = User();
   late List<ChildItem> _children;
@@ -44,19 +45,26 @@ class _HomeState extends State<Home> {
     super.initState();
     initForegroundTask();
     _children = [
-      ChildItem(title: 'Profile', widget: const Profile()),
       ChildItem(
-          title: 'Bot Channels',
+        title: 'Profile',
+        widget: const Profile(),
+      ),
+      ChildItem(
+          title: 'Bot Channel',
           widget: BotChannels(
             reloadUserData: _loadUserData,
           )),
       ChildItem(
-          title: 'Locations',
+        title: 'Tracking',
+        widget: const Tracking(),
+      ),
+      ChildItem(
+          title: 'Location',
           widget: Locations(
             reloadUserData: _loadUserData,
           )),
       ChildItem(
-        title: 'Aliases',
+        title: 'Alias',
         widget: Aliases(reloadUserData: _loadUserData),
       ),
     ];
@@ -114,7 +122,7 @@ class _HomeState extends State<Home> {
   }
 
   Future<void> _onAddButtonTapped(BuildContext parentContext) async {
-    if (_currentIndex == 3) {
+    if (_currentIndex == 4) {
       return showDialog(
         context: parentContext,
         builder: (BuildContext context) {
@@ -182,31 +190,36 @@ class _HomeState extends State<Home> {
           bottomNavigationBar: BottomNavigationBar(
             currentIndex: _currentIndex,
             onTap: _onBottomNavTapped,
-            items: const [
+            items: [
               BottomNavigationBarItem(
-                icon: Icon(Icons.person_outlined),
-                activeIcon: Icon(Icons.person),
-                label: 'Profile',
+                icon: const Icon(Icons.person_outlined),
+                activeIcon: const Icon(Icons.person),
+                label: _children[0].title,
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.chat_outlined),
-                activeIcon: Icon(Icons.chat),
-                label: 'Bot Channels',
+                icon: const Icon(Icons.chat_outlined),
+                activeIcon: const Icon(Icons.chat),
+                label: _children[1].title,
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.location_on_outlined),
-                activeIcon: Icon(Icons.location_on),
-                label: 'Locations',
+                icon: const Icon(Icons.navigation_outlined),
+                activeIcon: const Icon(Icons.navigation),
+                label: _children[2].title,
               ),
               BottomNavigationBarItem(
-                icon: Icon(Icons.campaign_outlined),
-                activeIcon: Icon(Icons.campaign),
-                label: 'Aliases',
+                icon: const Icon(Icons.location_on_outlined),
+                activeIcon: const Icon(Icons.location_on),
+                label: _children[3].title,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.campaign_outlined),
+                activeIcon: const Icon(Icons.campaign),
+                label: _children[4].title,
               ),
             ],
             type: BottomNavigationBarType.fixed,
           ),
-          floatingActionButton: (_currentIndex == 2 || _currentIndex == 3)
+          floatingActionButton: (_currentIndex == 3 || _currentIndex == 4)
               ? FloatingActionButton(
                   child: const Icon(Icons.add),
                   onPressed: () => _onAddButtonTapped(context),
