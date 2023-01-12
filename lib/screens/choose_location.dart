048505b2128454c3178e474241bcebd67e20fa99
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-01-12 16:11:03 +0900

    Optimize import

--- lib/screens/choose_location.dart
+++ lib/screens/choose_location.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
-// import 'package:location/location.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:trackme/utilities/custom_callback_types.dart';
 import 'package:trackme/utilities/api.dart';
